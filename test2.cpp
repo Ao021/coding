faab7b9367f6428ddd896e28d44ecbd1ce63954b
Ao021 <646194043@qq.com>
2022-08-29 18:57:42 +0800

    delete[]的作用和静态成员变量的使用

--- test2.cpp
+++ test2.cpp
@@ -1,1 +1,18 @@
 //nothing
+#include <iostream>
+using namespace std;
+
+class A{
+public:
+    static int num;
+    int a;
+    ~A(){cout<<"delete"<<A::num++<<endl;}
+};
+
+int A::num = 0;
+
+int main(){
+    A* arr = new A[10];
+    delete[] arr;
+    return 0;
+}
